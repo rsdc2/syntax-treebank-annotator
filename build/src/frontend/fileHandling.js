4b1b3239dbc99755badd6971e353510319aded89
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2023-03-29 15:46:26 +0100

    add filesaver api loads arethusa, text and epidoc from file

--- build/src/frontend/fileHandling.js
+++ build/src/frontend/fileHandling.js
@@ -1,30 +1,49 @@
 var FileHandling;
 (function (FileHandling) {
-    const saveFile = () => {
-    };
-    const files = (elem) => {
-        return MaybeT.of(elem.files);
-    };
-    const fileInputChange = (e) => {
-        MaybeT.of(e.target)
-            .bind(files)
-            .fmap(Arr.fromIterable)
-            .bind(Arr.head)
-            .fmap(processTextFile(Frontend.processArethusa)('UTF-8'));
-    };
-    FileHandling.loadFile = () => {
+    FileHandling.loadFromDialog = (callback) => {
         const fileInput = document.createElement('input');
         fileInput.type = "file";
-        fileInput.onchange = fileInputChange;
+        fileInput.onchange = InputElem.onchange(callback);
         fileInput.click();
     };
-    const processTextFile = (f) => (encoding) => (blob) => {
-        const reader = new FileReader();
-        reader.readAsText(blob, encoding);
-        reader.onload = (e) => {
-            const target = e.target;
-            const fileContent = MaybeT.of(target === null || target === void 0 ? void 0 : target.result);
-            fileContent.fmap(f);
-        };
+    FileHandling.saveToDialog = () => {
+        const handle = showSaveFilePicker();
     };
+    let TextFile;
+    (function (TextFile) {
+        // export const write = async (fileHandle: FileSystemFileHandle) => (blob: Blob) => {
+        //     const writer = fileHandle.createWritable();
+        //     await writer.write(blob);
+        //     await writer.close();
+        // }
+        TextFile.process = (f) => (encoding) => (blob) => {
+            const reader = new FileReader();
+            reader.readAsText(blob, encoding);
+            reader.onload = (e) => {
+                const fileContent = MaybeT.of(e.target)
+                    .bind(FR.result);
+                // TODO handle array buffer
+                fileContent.applyFmap(f);
+            };
+        };
+    })(TextFile = FileHandling.TextFile || (FileHandling.TextFile = {}));
+    let FR;
+    (function (FR) {
+        FR.result = (fr) => {
+            return MaybeT.of(fr.result);
+        };
+    })(FR = FileHandling.FR || (FileHandling.FR = {}));
+    let InputElem;
+    (function (InputElem) {
+        InputElem.files = (elem) => {
+            return MaybeT.of(elem.files);
+        };
+        InputElem.onchange = (callback) => (e) => {
+            MaybeT.of(e.target)
+                .bind(InputElem.files)
+                .fmap(Arr.fromIterable)
+                .bind(Arr.head)
+                .fmap(TextFile.process(callback)('UTF-8'));
+        };
+    })(InputElem = FileHandling.InputElem || (FileHandling.InputElem = {}));
 })(FileHandling || (FileHandling = {}));
--- src/frontend/fileHandling.ts
+++ src/frontend/fileHandling.ts
@@ -1,43 +1,52 @@
 namespace FileHandling {
-
-    const saveFile = () => {
-        
-    }
     
-    const files = (elem: HTMLInputElement) => {
-        return MaybeT.of(elem.files);
-    }
-
-    const fileInputChange = (e: Event) => {
-        MaybeT.of(e.target as HTMLInputElement | null)
-            .bind(files)
-            .fmap(Arr.fromIterable)
-            .bind(Arr.head)
-            .fmap(processTextFile(Frontend.processArethusa)('UTF-8'))
-    }
-
-    export const loadFile = () => {
+    export const loadFromDialog = (callback: Maybe<(a: string) => any>) => {
         const fileInput = document.createElement('input') as HTMLInputElement;
         fileInput.type = "file";
-        fileInput.onchange = fileInputChange;        
+        fileInput.onchange = InputElem.onchange(callback);        
         fileInput.click();
     }
 
-    const processTextFile = (f: (a: string) => any) => (encoding: string) => (blob: Blob) =>  {
-        const reader = new FileReader();
-        reader.readAsText(blob, encoding);
-
-        reader.onload = (e: ProgressEvent<FileReader>) => {
-            const fileContent = MaybeT.of(e.target)
-                .bind(FR.result);
-            fileContent.fmap(f)
-        }        
+    export namespace TextFile {
+        export const process = 
+            (f: Maybe<(a: string) => any>) => 
+            (encoding: string) => 
+            (blob: Blob) =>  
+        
+        {
+            const reader = new FileReader();
+            reader.readAsText(blob, encoding);
+    
+            reader.onload = (e: ProgressEvent<FileReader>) => {
+                const fileContent = MaybeT.of(e.target)
+                    .bind(FR.result);
+    
+                // TODO handle array buffer
+                fileContent.applyFmap(f)
+            }        
+        }
+    
     }
 
     export namespace FR {
         export const result = (fr: FileReader) => {
             return MaybeT.of(fr.result)
         }    
+    }
+
+    export namespace InputElem {
+        export const files = (elem: HTMLInputElement) => {
+            return MaybeT.of(elem.files);
+        }
+
+        export const onchange = (callback: Maybe<(a: string) => any>) => (e: Event) => {
+            MaybeT.of(e.target as HTMLInputElement | null)
+                .bind(InputElem.files)
+                .fmap(Arr.fromIterable)
+                .bind(Arr.head)
+                .fmap(TextFile.process (callback) ('UTF-8'))
+        }
+        
     }
 
 
