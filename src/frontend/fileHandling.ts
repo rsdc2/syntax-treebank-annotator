3907891199d60251cd7ba3490ca5b08265e0ec84
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2023-05-15 08:51:45 +0100

    comment references for file handling

--- src/frontend/fileHandling.ts
+++ src/frontend/fileHandling.ts
@@ -1,3 +1,7 @@
+// For the underlying procedures (minus functional overlay)
+// see https://github.com/papyrs/markdown-plugin/blob/main/src/plugin/utils/save.utils.ts (MIT license)
+// For an explanation, see also https://itnext.io/export-to-the-file-system-save-as-fallback-in-typescript-6561eba853cb
+
 namespace FileHandling {
 
     export const download = 
