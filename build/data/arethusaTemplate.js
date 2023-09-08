593430a9d2e7acbb10079f6514577df5da4ade61
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2023-09-08 13:00:31 +0100

    removed version information from arethusa template

--- build/data/arethusaTemplate.js
+++ build/data/arethusaTemplate.js
@@ -1,10 +1,10 @@
 const arethusaTemplate = `<?xml version="1.0" encoding="UTF-8"?>
-<treebank xml:lang="" format="aldt" version="1.5">
+<treebank xml:lang="">
     <annotator name="" address="" role="" date="" layer=""/>
     <document_meta/>
 </treebank>`;
 const arethusaUglifiedExample = `<?xml version="1.0" encoding="UTF-8"?>
-<treebank xml:lang="la" format="aldt" version="1.5">
+<treebank xml:lang="la">
 <annotator name="" address="" role="" date="" layer=""/>
 <sentence id="1" document_id="ISic000001">	<word id="1" form="Dis" lemma="" postag="" relation="" head=""/>
 <word id="2" form="manibus" lemma="" postag="" relation="" head=""/>
