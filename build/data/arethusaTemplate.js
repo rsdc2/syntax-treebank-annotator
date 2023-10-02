6aae7ea64ed20b4d7f8264071e50ec8e6ea057a1
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2023-10-02 17:37:16 +0100

    removed version number

--- build/data/arethusaTemplate.js
+++ build/data/arethusaTemplate.js
@@ -1,10 +1,10 @@
 const arethusaTemplate = `<?xml version="1.0" encoding="UTF-8"?>
-<treebank xml:lang="">
+<treebank xml:lang="" format="aldt">
     <annotator name="" address="" role="" date="" layer=""/>
     <document_meta/>
 </treebank>`;
 const arethusaUglifiedExample = `<?xml version="1.0" encoding="UTF-8"?>
-<treebank xml:lang="la">
+<treebank xml:lang="la" format="aldt">
 <annotator name="" address="" role="" date="" layer=""/>
 <sentence id="1" document_id="ISic000001">	<word id="1" form="Dis" lemma="" postag="" relation="" head=""/>
 <word id="2" form="manibus" lemma="" postag="" relation="" head=""/>
