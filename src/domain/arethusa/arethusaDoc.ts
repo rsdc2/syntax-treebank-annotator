87120349ef81ceecd85e24c907660edb15a97493
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2023-10-03 17:49:48 +0100

    formatting

--- src/domain/arethusa/arethusaDoc.ts
+++ src/domain/arethusa/arethusaDoc.ts
@@ -633,7 +633,6 @@ class ArethusaDoc implements ArethusaSentenceable, HasToken {
 
                 // If current head is root, or if renumberHeads is set to false
                 // do not renumber
-
                 const newHeadIdStr = renumberHeads === true && currentHeadIdInt !== 0 && Number.isNaN(currentHeadIdInt) === false 
                     ? Str.fromNum(currentHeadIdInt + offset) 
                     : currentHeadIdStr
