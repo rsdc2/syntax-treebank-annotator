dc2ac72a07347ce283f90e42c80f0924cc2b7c84
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2023-10-04 11:36:57 +0100

    removed layer attr from xml

--- build/data/arethusaTemplate.js
+++ build/data/arethusaTemplate.js
@@ -1,11 +1,11 @@
 const arethusaTemplate = `<?xml version="1.0" encoding="UTF-8"?>
 <treebank xml:lang="" format="aldt">
-    <annotator name="" address="" role="" date="" layer=""/>
+    <annotator name="" address="" role="" date=""/>
     <document_meta/>
 </treebank>`;
 const arethusaUglifiedExample = `<?xml version="1.0" encoding="UTF-8"?>
 <treebank xml:lang="la" format="aldt">
-<annotator name="" address="" role="" date="" layer=""/>
+<annotator name="" address="" role="" date=""/>
 <sentence id="1" document_id="ISic000001">	<word id="1" form="Dis" lemma="" postag="" relation="" head=""/>
 <word id="2" form="manibus" lemma="" postag="" relation="" head=""/>
 <word id="3" form="Zethi" lemma="" postag="" relation="" head=""/>
