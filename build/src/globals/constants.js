8ac1292af4969216a6b4094e0ad0507d42191c2c
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2024-01-25 19:03:20 +0000

    latest about message

--- build/src/globals/constants.js
+++ build/src/globals/constants.js
@@ -32,14 +32,19 @@ var Constants;
     };
     Constants.defaultRel = "rel";
     Constants.defaultViewBox = "0 0 560 560";
+    Constants.MAXTOKENS = 200;
+    /**
+     * Maximum file size in KB
+     */
+    Constants.MAXFILESIZE = 50;
     Constants.messages = {
-        about: String.raw `<p>
-                The Treebank Annotator was written by Robert Crellin as part of the
+        about: String.raw `The Treebank Annotator was written by Robert Crellin as part of the
                 Crossreads project at the Faculty of Classics, University of Oxford, and
-                is licensed under the MIT license. This project has received funding
+                is licensed under the MIT license 
+                (https://github.com/rsdc2/syntax-treebank-annotator/blob/main/LICENSE). 
+                This project has received funding
                 from the European Research Council (ERC) under the European Union's
                 Horizon 2020 research and innovation programme (grant agreement No
-                885040, "Crossreads").
-            </p>`
+                885040, "Crossreads").`
     };
 })(Constants || (Constants = {}));
