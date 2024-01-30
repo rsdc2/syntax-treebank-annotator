013a17de3006f4bc01461f061fcad47fa2746070
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2024-01-30 20:43:02 +0000

    handleSpecific errors

--- src/errors/errorHandler.js
+++ src/errors/errorHandler.js
@@ -26,4 +26,24 @@ class ErrorHandler {
 
     }
 
+    /**
+     * 
+     * @param {Array.<typeof Error>} errors 
+     * @param {Error} e
+     * @returns {boolean} 
+     */
+    static handleSpecific (errors, e) {
+        const outputArethusaDiv = ArethusaDiv.control_
+
+        errors.forEach(
+            (error) => {
+                if (e instanceof error) {
+                    outputArethusaDiv.replaceChildren(e.message)           
+                }
+            }
+        )
+        return false
+    }
+
+
 }
