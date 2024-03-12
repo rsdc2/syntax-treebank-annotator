5e971e4d2b78615ee7be1090ae7bdd89d778f77d
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2024-03-12 15:15:07 +0000

    better formatting

--- src/frontend/fileHandling.ts
+++ src/frontend/fileHandling.ts
@@ -5,72 +5,68 @@
 
 namespace FileHandling {
 
-    export const download = 
+    export const download =
         (filename: string) =>
-        (fileContent: string) => 
-    {
-        const url = MaybeT.of(fileContent)
-            .fmap(Str.blobify)
-            .fmap(HTML.URL.createObjectURL)
+            (fileContent: string) => {
+                const url = MaybeT.of(fileContent)
+                    .fmap(Str.blobify)
+                    .fmap(HTML.URL.createObjectURL)
 
-        const setUrl = url.fmap(HTML.Elem.setAttr("href"))
+                const setUrl = url.fmap(HTML.Elem.setAttr("href"))
 
-        HTML.Elem
-            .create('a')
-            .applyFmap(setUrl)
-            .fmap(HTML.Elem.setAttr("download")(`${filename}.xml`))
-            .fmap(HTML.Elem.click)
-            .fmap(HTML.Elem.remove)
-      
-        url.fmap(HTML.URL.revokeObjectURL);
-    }
+                HTML.Elem
+                    .create('a')
+                    .applyFmap(setUrl)
+                    .fmap(HTML.Elem.setAttr("download")(`${filename}.xml`))
+                    .fmap(HTML.Elem.click)
+                    .fmap(HTML.Elem.remove)
+
+                url.fmap(HTML.URL.revokeObjectURL);
+            }
 
     /**
      * 
      * @param ext file extension including '.'
      * @returns 
      */
-    export const loadFromDialog = 
+    export const loadFromDialog =
         (ext: string) =>
-        (callback: (a: string) => any) => 
-    {
-        const onchangeFunc = FileInput.onchange(callback, ext)
-        const setOnChangeFunc = HTML.Elem.setOnChangeFunc(onchangeFunc)
+            (callback: (a: string) => any) => {
+                const onchangeFunc = FileInput.onchange(callback, ext)
+                const setOnChangeFunc = HTML.Elem.setOnChangeFunc(onchangeFunc)
 
-        HTML.Elem
-            .create("input")
-            .fmapErr("Error", HTML.Elem.setAttr("type")("file"))
-            .fmapErr("Error", HTML.Elem.setAttr("accept")(ext))
-            .fmap(setOnChangeFunc)
-            .fmapErr("Error", HTML.Elem.click)
-            .fmap(HTML.Elem.remove)
-    }
+                HTML.Elem
+                    .create("input")
+                    .fmapErr("Error", HTML.Elem.setAttr("type")("file"))
+                    .fmapErr("Error", HTML.Elem.setAttr("accept")(ext))
+                    .fmap(setOnChangeFunc)
+                    .fmapErr("Error", HTML.Elem.click)
+                    .fmap(HTML.Elem.remove)
+            }
 
     export namespace TextFile {
-        export const process = 
-            (processString: (a: string) => any) => 
-            (encoding: string, ext: string) => 
-            (file: File) =>  
-        
-        {
-            FileValidator.assertCorrectExt(file, ext)
-            const reader = new FileReader();
-            reader.readAsText(file, encoding);
-    
-            reader.onload = (e: ProgressEvent<FileReader>) => {
-                const fileContent = e.target?.result as string;
-    
-                // TODO handle array buffer
-                processString(fileContent)
-            }        
-        }
-    
+        export const process =
+            (processString: (a: string) => any) =>
+                (encoding: string, ext: string) =>
+                    (file: File) => {
+                        FileValidator.assertCorrectExt(file, ext)
+                        const reader = new FileReader();
+                        reader.readAsText(file, encoding);
+
+                        reader.onload = (e: ProgressEvent<FileReader>) => {
+                            const fileContent = e.target?.result as string;
+
+                            // TODO handle array buffer
+                            processString(fileContent)
+                        }
+                    }
+
     }
 
     export namespace FR {
         export const result = (fr: FileReader) => {
             return MaybeT.of(fr.result)
-        }    
+        }
     }
 
     export namespace FileInput {
@@ -90,14 +86,14 @@ namespace FileHandling {
                 .bind(FileInput.files)
                 .fmap(Arr.fromIterable)
                 .bind(Arr.head)
-            
+
             try {
                 if (file.value?.size !== undefined && file.value.size < Constants.MAXFILESIZE * 1000) {
-                    file.fmap(TextFile.process (callback) ('UTF-8', ext))
+                    file.fmap(TextFile.process(callback)('UTF-8', ext))
                 } else {
                     throw new FileSizeError(
-                        `File is too large. ` + 
-                        `Files must be smaller than ` + 
+                        `File is too large. ` +
+                        `Files must be smaller than ` +
                         `${Constants.MAXFILESIZE}KB`
                     )
                 }
@@ -106,11 +102,11 @@ namespace FileHandling {
                     FileSizeError,
                     FileError
                 ], error
-            )
+                )
             }
 
         }
-        
+
     }
 
 
